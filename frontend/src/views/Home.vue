b6f8acd53da807a651c7d87640ddc634354113b4
Joachim Holwech <joachim.holwech@gmail.com>
2020-03-18 08:35:40 +0100

    Implemented a basic setup for showing graphs on a website

---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <div class="headline">
+      <h1>Corona status</h1>
+    </div>
+    <div class="container">
+      <div class="row">
+        <div class="col-6" id="plot"></div>
+        <div class="col-6"></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import plotly from 'plotly.js-dist';
+import plots from '../plots/test_corona.json';
+import Vue from 'vue';
+import Component from 'vue-class-component';
+
+@Component
+export default class Home extends Vue {
+  mounted() {
+    console.log(plots);
+    const plot = JSON.parse(plots['test']);
+    plot['config'] = {
+      responsive: true,
+    }
+    plotly.newPlot('plot', plot);
+  }
+}
+</script>
+
+<style>
+.item-style {
+  height: 500px;
+}
+</style>
